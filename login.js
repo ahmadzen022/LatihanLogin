8545575c336a11ece5ed608ec0378e9515f5bc86
ahmadzen022 <141709205+ahmadzen022@users.noreply.github.com>
2023-10-04 14:48:52 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+function login(username, pass) {
+  console.log("----------- ");
+  console.log("Login");
+  console.log("----------- ");
+  console.log("Username :" + username);
+  console.log("Pass :" + pass);
+  console.log("----------- ");
+}
+
+login("zeen", "********");
